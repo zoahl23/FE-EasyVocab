b592e98ca08a6e025549899e1f00d94357c68c80
zoahl23 <hlan22102003@gmail.com>
2024-09-13 15:53:56 +0700

    first commit

---
+++ src/components/home/index.jsx
@@ -0,0 +1,16 @@
+import './home.scss'
+
+const Home = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="helo">helo</div>
+                <div className="abc">abc</div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/home/index.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
