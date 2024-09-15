fddd1aefc6837d3e126fe1fe8e44b0f8e7457584
zoahl23 <hlan22102003@gmail.com>
2024-09-15 14:56:18 +0700

    Error Page with Antd

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,15 +1,17 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import { Button, Result } from 'antd';
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <Result
+            status="403"
+            title="Oops!"
+            subTitle={error.statusText || error.message}
+            extra={<Button type="primary">
+                <Link to="/"><span>Back to homepage</span></Link>
+            </Button>}
+        />
     );
 }
