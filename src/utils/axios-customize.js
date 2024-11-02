9cdb30a31c43b62b9a4bb59a25920475a3dde558
zoahl23 <hlan22102003@gmail.com>
2024-11-03 06:29:52 +0700

    tat refresh token trong axio-customize

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -5,13 +5,13 @@ const instance = axios.create({
     withCredentials: true,
 });
 
-instance.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem('access_token')}` }
+instance.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
 
-const handleRefreshToken = async () => {
-    const res = await instance.get('/api/v1/auth/refresh');
-    if (res && res.data) return res.data.access_token;
-    else return null;
-}
+// const handleRefreshToken = async () => {
+//     const res = await instance.get('/api/v1/auth/refresh');
+//     if (res && res.data) return res.data.access_token;
+//     else return null;
+// }
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
@@ -22,36 +22,36 @@ instance.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 
-const NO_RETRY_HEADER = 'x-no-retry'
+// const NO_RETRY_HEADER = 'x-no-retry'
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response.data && response.data.data ? response.data : response;
-}, async function (error) {
+}, function (error) { // async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    if (error.config && error.response
-        && +error.response.status === 401
-        && !error.config.headers[NO_RETRY_HEADER]
-    ) {
-        const access_token = await handleRefreshToken();
-        error.config.headers[NO_RETRY_HEADER] = 'true'
-        if (access_token) {
-            error.config.headers['Authorization'] = `Bearer ${access_token}`;
-            localStorage.setItem('access_token', access_token);
-            return instance.request(error.config);
-        }
-    }
+    // if (error.config && error.response
+    //     && +error.response.status === 401
+    //     && !error.config.headers[NO_RETRY_HEADER]
+    // ) {
+    //     const access_token = await handleRefreshToken();
+    //     error.config.headers[NO_RETRY_HEADER] = 'true'
+    //     if (access_token) {
+    //         error.config.headers['Authorization'] = `Bearer ${access_token}`;
+    //         localStorage.setItem('access_token', access_token);
+    //         return instance.request(error.config);
+    //     }
+    // }
 
-    if (error.config && error.response
-        && +error.response.status === 400
-        && error.config.url === '/api/v1/auth/refresh'
-    ) {
-        if (window.location.pathname !== '/')
-            window.location.href = '/login';
-    }
+    // if (error.config && error.response
+    //     && +error.response.status === 400
+    //     && error.config.url === '/api/v1/auth/refresh'
+    // ) {
+    //     if (window.location.pathname !== '/')
+    //         window.location.href = '/login';
+    // }
 
     return error?.response?.data ?? Promise.reject(error);
 });
