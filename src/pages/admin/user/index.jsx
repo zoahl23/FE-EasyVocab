423827bf5444d4e52e5bc9cb51fb1129554d8fee
zoahl23 <hlan22102003@gmail.com>
2024-11-03 17:08:10 +0700

    ui: user management design

---
+++ src/pages/admin/user/index.jsx
@@ -0,0 +1,11 @@
+import UserTable from "../../../components/Admin/User/UserTable";
+
+const ManageUserPage = () => {
+    return (
+        <div>
+            <UserTable />
+        </div>
+    );
+}
+
+export default ManageUserPage;
