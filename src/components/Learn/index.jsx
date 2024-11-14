d4b915afb541018950a5fa968d5b866f8aa12d62
zoahl23 <hlan22102003@gmail.com>
2024-11-14 23:56:38 +0700

    ui: design page learn course

--- src/components/Learn/index.jsx
+++ src/components/Learn/index.jsx
@@ -1,9 +1,68 @@
 import './style.scss';
 import MainContent from '../MainContent';
+import { Card, Col, Row } from 'antd';
+import { FcApproval, FcGraduationCap } from 'react-icons/fc';
 
 const Learn = () => {
+
+    const courseList = [
+        {
+            title: "PHRASAL VERB & IDIOM",
+            description1: "Tăng khả năng vận dụng tiếng Anh",
+            description2: "1400 Phrasal Verbs & Idioms thông dụng theo chủ đề",
+        },
+        {
+            title: "TIẾNG ANH CÔNG SỞ",
+            description1: "Giao tiếp môi trường công sở",
+            description2: "Từ vựng môi trường công sở",
+        },
+        {
+            title: "TIẾNG ANH CÔNG SỞ",
+            description1: "Giao tiếp môi trường công sở",
+            description2: "Từ vựng môi trường công sở",
+        },
+        {
+            title: "TIẾNG ANH CÔNG SỞ",
+            description1: "Giao tiếp môi trường công sở",
+            description2: "Từ vựng môi trường công sở",
+        },
+        {
+            title: "TIẾNG ANH CÔNG SỞ",
+            description1: "Giao tiếp môi trường công sở",
+            description2: "Từ vựng môi trường công sở",
+        },
+        {
+            title: "TIẾNG ANH CÔNG SỞ",
+            description1: "Giao tiếp môi trường công sở",
+            description2: "Từ vựng môi trường công sở",
+        },
+    ];
+
     return (
-        <MainContent>learn</MainContent>
+        <MainContent>
+            <Row gutter={[32, 32]} className="course-list">
+                <Col span={24} style={{ padding: 0 }}>
+                    <div className="rounded-card">
+                        1000 TỪ CƠ BẢN
+                    </div>
+                </Col>
+                {courseList.map((course, index) => (
+                    <Col span={24} key={index} style={{ padding: 0 }}>
+                        <Card className="course-card">
+                            <h3 className="course-title">{course.title}</h3>
+                            <div className="course-description">
+                                <FcApproval className="course-icon" />
+                                <span>{course.description1}</span>
+                            </div>
+                            <div className="course-description">
+                                <FcGraduationCap className="course-icon" />
+                                <span>{course.description2}</span>
+                            </div>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </MainContent >
     );
 }
 
